9f8d5b1452977a079a17ba77fdcca735b4e188bf
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-09 23:11:46 +0100

    Java Core : Data Types

---
+++ src/main/java/com/norcorp/datatypes/DataTypesInJava.java
@@ -0,0 +1,51 @@
+package com.norcorp.datatypes;
+
+public class DataTypesInJava {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA DATA TYPES ☕ ====================================");
+        //System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        /*
+            DataTypes = Primitive et Non-primitive
+            Primitive : (Integer, Float, Character, Boolean).
+            Integer ====> int, byte, short, long
+            Float ======> double, float
+            Character ==> char
+            Boolean ====> true or false
+
+            Non-primitive = (String, Arrays and Class)
+        */
+
+        // Integer
+        int myNum = 9;
+        byte myByte = 127;
+        short myShort = 558;
+        long myLongNumber = 5854l;
+
+        // Float
+        double myDouble = 6.5;
+        float marks = 6.5f;
+
+        // Character
+        char myLetter = 'C';
+
+        // Boolean
+        boolean myBoolean = true;
+
+
+
+        System.out.println();
+        System.out.println(myNum);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myLongNumber);
+        System.out.println(myDouble);
+        System.out.println(marks);
+        System.out.println(myLetter);
+        System.out.println(myBoolean);
+        System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
