1ad6e6430a46229504d5b0a002d5874b4ac9a136
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-09 23:25:49 +0100

    Java Core : Literals

---
+++ src/main/java/com/norcorp/literals/LiteralsInJava.java
@@ -0,0 +1,40 @@
+package com.norcorp.literals;
+
+public class LiteralsInJava {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA LITERALS ☕ ====================================");
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        /*
+            In Java, a literal is a representation of a fixed value in source code.
+            It is a constant value that can be assigned directly to a variable
+            or used as an operand in an expression.
+        */
+
+        // Integer Literals:
+        int intValue = 42;
+        // Floating-Point Literals:
+        double doubleValue = 3.14;
+        // Character Literals:
+        char charValue = 'A';
+        // String Literals:
+        String stringValue = "Hello";
+        // Boolean Literals:
+        boolean booleanValue = true;
+        /*
+            Null Literal: The null literal represents the absence of a value and is used for object references.
+            MyClass myObject = null;
+        */
+
+
+        System.out.println();
+        System.out.println(intValue);
+        System.out.println(doubleValue);
+        System.out.println(charValue);
+        System.out.println(stringValue);
+        System.out.println(booleanValue);
+        System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
