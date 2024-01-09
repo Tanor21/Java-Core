830b8c2043484bf91bdc98a0d952c1ff99e61f24
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-09 18:14:39 +0100

    Java Core : Variables

---
+++ src/main/java/com/norcorp/variables/VariablesInJava.java
@@ -0,0 +1,20 @@
+package com.norcorp.variables;
+
+public class VariablesInJava {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA VARIABLES ☕ ====================================");
+        //System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        // Variable = Type + Variable's name + initialisation;
+        int num1 = 9;
+        int num2 = 9;
+        int result = num1 + num2;
+
+        System.out.println();
+        System.out.println(result);
+        System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
