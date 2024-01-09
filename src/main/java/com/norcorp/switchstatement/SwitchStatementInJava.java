20e7677c4c414a715fe2f53ca14d9d1cb49fc023
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-10 00:11:48 +0100

    Java Core : Switch Statement

---
+++ src/main/java/com/norcorp/switchstatement/SwitchStatementInJava.java
@@ -0,0 +1,43 @@
+package com.norcorp.switchstatement;
+
+public class SwitchStatementInJava {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA SWITCH STATEMENT ☕ ====================================");
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        /*
+            Instead of writing many if..else statements, you can use the switch statement.
+            The switch statement selects one of many code blocks to be executed :
+         */
+        int day = 4;
+
+
+        System.out.println();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+        System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
