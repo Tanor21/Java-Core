68e6cd437c321ddc06c7e5766a6f8849683da756
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-10 10:51:05 +0100

    Java Core : Variables

--- src/main/java/com/norcorp/variables/VariablesInJava.java
+++ src/main/java/com/norcorp/variables/VariablesInJava.java
@@ -6,6 +6,16 @@ public class VariablesInJava {
         System.out.println("==================================== ☕ JAVA VARIABLES ☕ ====================================");
         System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
 
+        /*
+            Variables are containers for storing data values.
+            In Java, there are different types of variables, for example:
+            String - stores text, such as "Hello". String values are surrounded by double quotes
+            int - stores integers (whole numbers), without decimals, such as 123 or -123
+            float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+            char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+            boolean - stores values with two states: true or false
+         */
+
         // Variable = Type + Variable's name + initialisation;
         int num1 = 9;
         int num2 = 9;
