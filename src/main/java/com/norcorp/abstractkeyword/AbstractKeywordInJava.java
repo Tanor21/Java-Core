968e97329e2d24c29d993a6320f9586a10af15bb
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-14 18:44:56 +0100

    Java Core : Abstract Keyword

---
+++ src/main/java/com/norcorp/abstractkeyword/AbstractKeywordInJava.java
@@ -0,0 +1,35 @@
+package com.norcorp.abstractkeyword;
+
+abstract class Car {
+    public abstract void drive();
+
+    public void playMusic() {
+        System.out.println("Playing music...");
+    }
+}
+
+class Bmw extends Car {
+    public void drive() {
+        System.out.println("Driving...");
+    }
+}
+
+public class AbstractKeywordInJava {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA ABSTRACT ☕ ====================================");
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        /*
+           Abstract Keyword :
+
+         */
+        Car obj = new Bmw();
+
+
+        System.out.println();
+        obj.drive();
+        obj.playMusic();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
