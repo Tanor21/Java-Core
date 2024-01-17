457ee18b2aaba8dfaad68403588d9f34d69106fb
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-01-17 20:32:49 +0100

    Java Core : Var New Java 10 Features

---
+++ src/main/java/com/norcorp/varnewjava10/VarInJava.java
@@ -0,0 +1,30 @@
+package com.norcorp.varnewjava10;
+
+public class VarInJava {
+    // var num 10; // Error
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================================== ☕ JAVA VARIABLES IN JAVA 10 (NEW) ☕ ====================================");
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+
+        /*
+            Variable in Java 10 (New) :
+            Local Variable Type Inference (LVTI)
+         */
+        // int a = 10;
+        // var b = 8;
+        // String var = "Tanor";
+
+        // int c;
+        // var d = 10;
+
+        // var nums = new int[10];
+
+
+        System.out.println();
+
+        System.out.println();
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️️");
+    }
+}
